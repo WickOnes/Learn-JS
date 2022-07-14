b75089ef9dd9aa6e7ae2b3da621f486db91ab872
Nikita Lozoviy <niklozoviy@gmail.com>
2022-07-14 20:52:45 +0300

    finish

--- server-14/index.js
+++ server-14/index.js
@@ -1,10 +1,11 @@
 "use strict";
 const buttonPersonag = document.querySelector(".btn");
 const buttonPlanet = document.querySelector(".btnPlanet");
-const buttonNext = document.querySelector(".next")
-const buttonPrevious = document.querySelector(".previous")
-
-let countPageApi = "https://swapi.dev/api/planets/?page=1"
+const buttonNext = document.querySelector(".next");
+const buttonPrevious = document.querySelector(".previous");
+const loading = document.querySelector(".load");
+let countPageApi = "https://swapi.dev/api/planets/?page=1";
+let blockContentArray = [];
 const main = document.createElement("div");
 main.className = "main";
 document.body.append(main);
@@ -27,83 +28,80 @@ const personag = [
   "Lobot",
 ];
 
-// --------------Функція персонажів фільму
-async function getPerson() {
-  //------очистка контенту
-  main.innerHTML = "";
-  // ------------ Перевірка введенного фільму, додавання фільму та очистка поля вводу
+// --------------Check films
+function checkEnterFilm() {
   const input = document.querySelector("#input");
-  if (input.value > 6 || input.value < 1) {
-    return alert("Такого фільму нема");
-  }
-  const people = await fetch(`https://swapi.dev/api/films/${input.value}/`);
-  input.value = "";
-  // ---------------------- сторінка загрузки
-  const loading = document.querySelector(".load");
+  if (input.value > 6 || input.value < 1) return alert("Такого фільму нема");
   loading.classList.remove("noload");
-  //   ---------------------------Формування блоків з персонажами вибранного фільму
-  const peopleJson = await people.json();
-  let blockContent = "";
-  for (let i = 0; i < peopleJson.characters.length; i++) {
-    const persona = await fetch(peopleJson.characters[i]);
+}
+
+// --------------print block personag
+async function addPersonagBlock(param) {
+  for (let i = 0; i < param.length; i++) {
+    const persona = await fetch(param[i]);
     const personaJson = await persona.json();
     let imgName = "NO IMG";
     personag.forEach((count) => {
       if (count === personaJson.name) imgName = personaJson.name;
     });
-    blockContent += `<div class = "blockStyle"> <img src="img/${imgName}.jpg"> <p>Name: ${personaJson.name}</p><p>Gender: ${personaJson.gender}</p><p>Birthday: ${personaJson.birth_year}</p> </div> `;
+    blockContentArray.push(
+      `<div class = 'blockStyle'> <img src="img/${imgName}.jpg"> <p>Name: ${personaJson.name}</p><p>Gender: ${personaJson.gender}</p><p>Birthday: ${personaJson.birth_year}</p> </div>`
+    );
   }
-  //------------видалення сторінки загрузки
-  loading.classList.add("noload");
-
-  //-----------формування сторінки
-  main.innerHTML = blockContent;
+  main.innerHTML = blockContentArray.join();
+  input.value = "";
 }
 
-// ---------------Функціія планет
+// --------------Get personag
+async function getPerson() {
+  checkEnterFilm();
+  main.innerHTML = "";
+  const people = await fetch(`https://swapi.dev/api/films/${input.value}/`);
+  const peopleJson = await people.json();
+  const peopleCharacters = peopleJson.characters;
+  await addPersonagBlock(peopleCharacters);
+  //------------deled load
+  loading.classList.add("noload");
+}
+
+// ---------------print block Planet
+function addBlockPlanetName (param){
+  let blockContent = "";
+  for (let i = 0; i < param.length; i++) {
+    const namePlanet = param[i].name;
+    blockContent += `<div class = "blockStylePlanet"><p>Name: ${namePlanet}</p></div> `;
+  }
+  main.innerHTML = blockContent;
+}
+// ---------------print block Planet
 async function getPlanet() {
-  const loading = document.querySelector(".load");
-  //------очистка контенту
+  loading.classList.remove("noload");
   main.innerHTML = "";
   const planet = await fetch(countPageApi);
   const planetJson = await planet.json();
   const planetName = await planetJson.results;
-  // ---------------------- сторінка загрузки
-
-  loading.classList.remove("noload");
-  //------------ формування сторінки з планетами
-  let blockContent = "";
-  for (let i = 0; i < planetName.length; i++) {
-    const namePlanet = planetName[i].name;
-    blockContent += `<div class = "blockStylePlanet"><p>Name: ${namePlanet}</p></div> `;
-  }
-  //------------видалення сторінки загрузки
+  addBlockPlanetName(planetName)
   loading.classList.add("noload");
 
-  //-----------формування сторінки
-  main.innerHTML = blockContent;
 }
 
-
-async function nextPage(){
-const pageApi = await fetch(countPageApi)
-const pageApiJson = await pageApi.json()
-if(!pageApiJson.next) return
-countPageApi = pageApiJson.next
-    getPlanet()
+async function nextPage() {
+  const pageApi = await fetch(countPageApi);
+  const pageApiJson = await pageApi.json();
+  if (!pageApiJson.next) return;
+  countPageApi = pageApiJson.next;
+  getPlanet();
 }
 
-async function previousPage(){
-    const pageApi = await fetch(countPageApi)
-    const pageApiJson = await pageApi.json()
-    if(!pageApiJson.previous) return
-    countPageApi = pageApiJson.previous
-        getPlanet()
-    }
-
-
+async function previousPage() {
+  const pageApi = await fetch(countPageApi);
+  const pageApiJson = await pageApi.json();
+  if (!pageApiJson.previous) return;
+  countPageApi = pageApiJson.previous;
+  getPlanet();
+}
 
 buttonPlanet.addEventListener("click", getPlanet);
-buttonNext.addEventListener('click',nextPage)
-buttonPrevious.addEventListener('click',previousPage)
+buttonNext.addEventListener("click", nextPage);
+buttonPrevious.addEventListener("click", previousPage);
 buttonPersonag.addEventListener("click", getPerson);
